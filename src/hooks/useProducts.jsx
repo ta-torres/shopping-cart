139506b187e4660f48627b4fbc3efb233ea6fea6
Thomas <93293510+ta-torres@users.noreply.github.com>
2025-01-16 20:12:26 -0300

    Fetch products from api

---
+++ src/hooks/useProducts.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+const useProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch("https://fakestoreapi.com/products");
+        const data = await response.json();
+        setProducts(data);
+        console.log(data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return { products, loading, error };
+};
+
+export default useProducts;
